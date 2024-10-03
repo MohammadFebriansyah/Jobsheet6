45050a961878c0fa59daf45a3c69b735e30eed29
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2024-10-03 18:49:42 +0700

    Percobaan 1 modifikasi 2

--- Pemilihan2Percobaan113.java
+++ Pemilihan2Percobaan113.java
@@ -9,12 +9,14 @@ public class Pemilihan2Percobaan113 {
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0){
-            System.out.println("Tahun Kabisat");
-            
-        }   else{ 
-            System.out.println("Bukan Tahun Kabisat");
-        }
-     } else { System.out.println("Bukan Tahun Kabisat");
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat"); 
+                }else{ 
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+             }else { System.out.println("Tahun Kabisat");
+            }
+        } else { System.out.println("Bukan Tahun Kabisat");
 
         }
         
